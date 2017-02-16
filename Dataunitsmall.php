37a07a5ff3794bf8b6f4e7c7a61a38792b39acb4
Herbert001 <herbert001sc@gmail.com>
2017-02-16 15:50:20 +0100

    diff changes

--- Dataunitsmall.php
+++ Dataunitsmall.php
@@ -15,7 +15,7 @@ if (empty($_SESSION['usr_id'])) {
         <title> DeTec UnitDataInfoSystem ALPHA #0.7.2</title>
         <meta name= "viewport" content = "width=device-width, initial-scale=1.0">
         <link href = "inc/css/bootstrap.css" rel = "stylesheet">
-        <link href = "inc/css/styles.css" rel = "stylesheet">
+        <link href = "inc/css/styles3.css" rel = "stylesheet">
         <link href = "inc/css/font-awesome.min.css" rel="stylesheet" />
         <script src= "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src ="inc/js/bootstrap.js"></script>
@@ -61,7 +61,7 @@ include 'inc/func/function.php';
 
 
 // Abfrage welche Identifikationsnummer(n) gehören zu der u_id Ausgabe im Jumbotron
-$idsFromUnit = ("SELECT a.id, a.u_id, a.ident_id, a.typ_id
+$idsFromUnit = ("SELECT a.id, a.u_id, a.ident_id AS ID, a.typ_id
 FROM unit a
 WHERE a.u_id = '" . $_SESSION['uids'] . "' AND a.u_visible = 1");
 
@@ -98,12 +98,12 @@ if ($result = $db->query($customer_unit_ds)) {                                  
           if ($result->num_rows) {
           $rows = $result->fetch_all(MYSQLI_ASSOC);
             foreach ($rows as $row) {
-            echo  "&nbsp <a href='/get_ident_nr.php?identid=" .$row['ident_id']."&submit=' >" .$row['ident_id']. "</a><br />";  //Ausgabe der IDs auf der rechten Seite im Jumbotron
-            }}
+              $anlagenID = $row['ID'];
+              echo  "&nbsp <a class='linkjumbo' href='/get_ident_nr.php?identid=" .$row['ID']."&submit=' >" .$row['ID']. "</a><br />";  //Ausgabe der IDs auf der rechten Seite im Jumbotron
+              }}
 }?>
 </div><br />
 </div> </div> </div>
-
 </div>
 <?php
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -113,7 +113,35 @@ if ($result = $db->query($customer_unit_ds)) {                                  
 switch ($row['typ_id']) {
     case 1:
         echo "i ist gleich Kälteanlage";
+      $refrigeration = TRUE;
+      $ausgabe_include =  'ausgabe_refrigeration.php';
+      $get_refrigeration ("SELECT a.ident_id, a.lastService AS Wartung, b.uIdentId, c.r_typ_id, c.r_sernr AS Seriennummer, c.r_build_year AS Baujahr,
+          c.r_vdkf AS VDKF, d.id, d.r_description AS
+          Bezeichnung, d.r_manufactor_nr,
+          d.r_fr_load AS FMenge, d.r_power AS KLeistung, e.name AS Hersteller, f.fr_name AS Frigen
+        FROM unit a JOIN unit_link_tab b ON a.ident_id = b.uIdentId
+        JOIN refrigeration c ON c.id = b.rIdentId
+        JOIN refrigeration_typ d ON c.r_typ_id = d.id
+        JOIN manufactor e ON d.r_manufactor_id = e.id
+        JOIN frigen f ON d.r_fr_id = f.fr_id
+        WHERE a.ident_id = '" . $anlagenID_id . "'");
+
+      if ($result = $db->query($get_refrigeration)) {
+          if ($result->num_rows) {
+          $rows = $result->fetch_all(MYSQLI_ASSOC);
+            foreach ($rows as $row) {
+              $bezeichnung    = $row['Bezeichnung'];
+              $hersteller     = $row['Hersteller'];
+              $seriennr       = $row['Seriennummer'];
+              $baujahr        = $row['Baujahr'];
+              $frigen         = $row['Frigen'];
+              $fuellmenge     = $row['FMenge'];
+              $kaelteleistung = $row['KLeistung'];
+              $lastmaintenance= $row['Wartung'];
+              $vdkf           = $row['VDKF'];
+        }}};
         break;
+
     case 2:
         echo "i ist Druckluftkompressor Schraube";
         break;
@@ -130,7 +158,8 @@ switch ($row['typ_id']) {
         echo "i ist Drucklufttrockner";
         break;
     case 7:
-        echo "i ist Kaltwassersatz";
+        $chiller = TRUE;
+      $ausgabe_include =  'ausgabe_chiller.php';
         // Abfrage Details wenn Chiller
         $get_chiller =("SELECT a.ident_id, a.lastService AS Wartung, b.uIdentId, c.c_typ_id, c.c_sernr AS Seriennummer, c.c_build_year AS Baujahr,
           c.c_vdkf AS VDKF, d.id, d.c_description AS
@@ -141,12 +170,13 @@ switch ($row['typ_id']) {
         JOIN chiller_typ d ON c.c_typ_id = d.id
         JOIN manufactor e ON d.c_manufactor_id = e.id
         JOIN frigen f ON d.c_fr_id = f.fr_id
-        WHERE a.ident_id = 'WM-NI2-1004'");
+        WHERE a.ident_id = '" . $anlagenID . "'");
+
         if ($result = $db->query($get_chiller)) {
           if ($result->num_rows) {
           $rows = $result->fetch_all(MYSQLI_ASSOC);
             foreach ($rows as $row) {
-              $bezeichnung     = $row['Bezeichnung'];
+              $bezeichnung    = $row['Bezeichnung'];
               $hersteller     = $row['Hersteller'];
               $seriennr       = $row['Seriennummer'];
               $baujahr        = $row['Baujahr'];
@@ -163,175 +193,16 @@ switch ($row['typ_id']) {
         echo "i ist Lüftungsanlage";
         break;
 }
+$queryAnlagentyp = ("SELECT ident_id, typ_id, typ_auswahl FROM `unit`
+JOIN unittypen b ON typ_id = b.id WHERE ident_id = '" . $anlagenID . "'");
+if ($result = $db->query($queryAnlagentyp)) {                                   //Abfrage welche Art ist die Anlage (Kompressor, Klima...)
+    if ($result->num_rows) {                                                    //und Ausgabe im Panel unter shortfacts
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        foreach ($rows as $row){
+          $anlagentypus = $row['typ_auswahl'];}}}
 
 
+          include $ausgabe_include;                                             // Panel links mit Daten aus Abfrage $get_chiller
+          include 'footer.php'                                                  // include per case noch unterscheiden
 ?>
-<div class= "container">
-<div class= "row">
-    <div class="col-md-5">
-        <div class="panel panel-success paneltop">
-            <div class="panel-heading">Anlagendaten<br /><h4 class="panel-title">short facts</h4>
-            </div>
 
-            <ul class="list-group">
-                <li class="list-group-item"><i class="fa fa-building-o fa-fw" aria-hidden="true"></i>&nbsp;Hersteller:<div class=pull-right>
-                        <!-- Ausgabe und Prüfung Hersteller Anfang -->
-                        <?php
-                        if ($hersteller != NULL) {
-                            echo $hersteller;
-                        } else {
-                            echo 'NO DATA AVAILABLE';
-                        }
-                        ?> </div>
-                        <!-- Ausgabe und Prüfung Hersteller Ende -->
-                </li>
-                                            <li class="list-group-item"><i class="fa fa-wpforms fa-fw" aria-hidden="true"></i>&nbsp;Typ:<div class=pull-right>
-                                                    <!-- Ausgabe und Prüfung Typ Anfang -->
-                                                    <?php
-                                                    if ($bezeichnung != NULL) {
-                                                        echo $bezeichnung;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Ausgabe und Prüfung Typ Ende -->
-                                            </li>
-
-                                            <li class="list-group-item"><i class="fa fa-hashtag fa-fw" aria-hidden="true"></i>&nbsp;Ser. Nr.:<div class=pull-right>
-                                                    <!-- Ausgabe und Prüfung Seriennummer Anfang -->
-                                                    <?php
-                                                    if ($seriennr != NULL) {
-                                                        echo $seriennr;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Ausgabe und Prüfung Seriennummer Ende -->
-                                            </li>
-
-                                            <li class="list-group-item"><i class="fa fa-calendar-o fa-fw" aria-hidden="true"></i>&nbsp;Baujahr:<div class=pull-right>
-                                                    <!-- Ausgabe und Prüfung Baujahr Anfang -->
-                                                    <?php
-                                                    if ($baujahr) {
-                                                        echo $baujahr;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Ausgabe und Prüfung Baujahr Ende -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-bitbucket fa-fw" aria-hidden="true"></i>&nbsp;Kältemittel:<div class=pull-right>
-                                                    <!-- Ausgabe und Prüfung Kältemittelbezeichung Anfang -->
-                                                    <?php
-                                                    if ($frigen != NULL) {
-                                                        echo $frigen;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Ausgabe und Prüfung Kältemittelbezeichung Ende -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-balance-scale fa-fw" aria-hidden="true"></i>&nbsp;Füllmenge:<div class=pull-right>
-                                                    <!-- Prüfung und Ausgabe KM- Menge ANFANG -->
-                                                    <?php
-                                                    if ($fuellmenge != NULL) {
-                                                        echo $fuellmenge;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Prüfung und Ausgabe KM- Menge ENDE -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-cogs fa-fw" aria-hidden="true"></i>&nbsp;Kälteleistung:<div class=pull-right>
-                                                    <!-- Prüfung und Ausgabe Kälteleistung ANFANG -->
-                                                    <?php
-                                                    if ($kaelteleistung != NULL) {
-                                                        echo $kaelteleistung;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Prüfung und Ausgabe Kälteleistung ENDE -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-edit fa-fw" aria-hidden="true"></i>&nbsp;VDKF:<div class=pull-right>
-                                                    <!-- Prüfung und Ausgabe VDKF ANFANG -->
-                                                    <?php
-                                                    if ($vdkf != NULL) {
-                                                        echo $vdkf;
-                                                    } else {
-                                                        echo 'NO DATA AVAILABLE';
-                                                    }
-                                                    ?></div>
-                                                    <!-- Prüfung und Ausgabe VDKF ENDE -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-file-text-o fa-fw" aria-hidden="true"></i>&nbsp;Wartungsvertrag:<div class=pull-right>
-                                                    <!-- Prüfung und Ausgabe Wartungsvertrag ANFANG -->
-                                                    <?php
-                                                    if ($lastmaintenance!= NULL) {
-                                                        echo $lastmaintenance;
-                                                    } else {
-
-                                                        echo "<span class='glyphicon glyphicon-remove'></span>";
-                                                    }
-                                                    ?></div>
-                                                    <!-- Prüfung und Ausgabe Wartungsvertrag ENDE -->
-                                            </li>
-                                            <li class="list-group-item"><i class="fa fa-industry fa-fw" aria-hidden="true"></i>&nbsp;Letzte Wartung:<div class=pull-right>
-                                                    <!-- Prüfung und Ausgabe Letzte Wartung ANFANG -->
-                                                    <?php
-                                                    if ($lastmaintenance != NULL) {
-                                                        echo $$lastmaintenance;
-                                                    } else {
-
-                                                        echo "<span class='glyphicon glyphicon-remove'></span>";
-                                                    }
-                                                    ?></div>
-                                                    <!-- Prüfung und Ausgabe Letzte Wartung ENDE -->
-                                            </li>
-                                            <li class="list-group-item">---------:</li>
-                                        </ul>
-                                    </div>
-                                </div>
-
-
-
-                                <div class="col-md-offset-1 col-md-6">
-                                    <div class="panel panel-default">
-                                        <div class="panel-heading">Historie</div>
-                                        <div class="panel-body panel-height">
-
-<?php
- /***********************
- * Ausgabe der Einträge
- ***********************/
-
-//Ermittelt die Anzahl der Beiträge
-$result_c = $db->query("SELECT COUNT(*) FROM historie WHERE u_id = '" . $_SESSION['uids'] . "'");
-$row = $result_c->fetch_row();
-echo '#: ', $row[0];
-
-//Berechne alles notwendige für die Blätterfunktion
-$entrysPerPage = 3;                                                             // Artikel pro Seite
-$pages = ceil($row[0]/$entrysPerPage);                                          // Berechne wieviel Seiten
-echo $pages;                                                                    // Anzahl Seiten
-                                                                                // Erste Seite
-echo "<p><a href='./'>".'[Start]'."</a> ";
-// For Schleife für Seitendurchlauf
-for ($i = 1; $i <= $pages; $i++) {
-    echo "<a href='?page=".$i."'>Seite ".$i."</a> ";
-}
-// Letzt Seite
-echo "<a href='?page=$pages'>".'[Ende]'."</a></p>";
-
-echo '</div>';
-
-// Nach Anschluß alle Aufgaben, SQL Verbindung schließen
-$db->close();
-
- ?></div>                                     </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                       <?php include 'footer.php' ?>
